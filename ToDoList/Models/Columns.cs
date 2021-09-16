ecb1eeecca9eb9adfe7af5f4791fe5998e38d247
Liubov <luba22www@gmail.com>
2021-09-16 17:13:48 +0300

    back-end

---
+++ ToDoList/Models/Columns.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToDoList
+{
+    public partial class Columns
+    {
+        public Columns()
+        {
+            Tasks = new HashSet<Tasks>();
+        }
+
+        public int ColumnId { get; set; }
+        public string ColumnName { get; set; }
+        public int BoardId { get; set; }
+
+        public virtual Boards Board { get; set; }
+        public virtual ICollection<Tasks> Tasks { get; set; }
+    }
+}
---
+++ ToDoList/Models/ToDoDBContext.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToDoList
+{
+    public partial class ToDoDBContext : DbContext
+    {
+        public ToDoDBContext()
+        {
+        }
+
+        public ToDoDBContext(DbContextOptions<ToDoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Boards> Boards { get; set; }
+        public virtual DbSet<Columns> Columns { get; set; }
+        public virtual DbSet<Tasks> Tasks { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<UsersBoards> UsersBoards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=ToDoDB;Persist Security Info=True;User ID=sa;Password=sa");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Boards>(entity =>
+            {
+                entity.HasKey(e => e.BoardId)
+                    .HasName("PK_Projects");
+
+                entity.Property(e => e.BoardName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Columns>(entity =>
+            {
+                entity.HasKey(e => e.ColumnId);
+
+                entity.Property(e => e.ColumnName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Board)
+                    .WithMany(p => p.Columns)
+                    .HasForeignKey(d => d.BoardId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Columns_Boards");
+            });
+
+            modelBuilder.Entity<Tasks>(entity =>
+            {
+                entity.HasKey(e => e.TaskId);
+
+                entity.Property(e => e.TaskDescription)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TaskName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Column)
+                    .WithMany(p => p.Tasks)
+                    .HasForeignKey(d => d.ColumnId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Tasks_Columns");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.HasIndex(e => e.UserName)
+                    .HasName("IX_Users")
+                    .IsUnique();
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<UsersBoards>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Users_Boards");
+
+                entity.HasOne(d => d.Board)
+                    .WithMany()
+                    .HasForeignKey(d => d.BoardId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Users_Projects2");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Users_Projects1");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
