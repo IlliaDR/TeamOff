0966aee3e98da939993381314e2363cb8595ec8f
Liubov <luba22www@gmail.com>
2021-09-24 22:11:30 +0300

    new backend

---
+++ ToDo/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToDo
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            Tasks = new HashSet<Tasks>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Tasks> Tasks { get; set; }
+    }
+}
